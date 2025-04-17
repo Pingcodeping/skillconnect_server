2fe3d18a5352f6787d9bef7c7c5bc3241e03d15c
Pingcodeping <Pingcodeping@gmail.com>
2025-04-17 18:04:15 +0530

    commit 1

--- app.js
+++ app.js
@@ -2,12 +2,22 @@ const express = require('express');
 const cors = require('cors');
 const userRoutes = require('./routes/userRoutes');
 const questionRoutes = require('./routes/questionRoutes');
-
+const path = require('path');
 const app = express();
+
 app.use(cors());
 app.use(express.json());
 
 app.use('/api/users', userRoutes);
 app.use('/api/questions', questionRoutes);
+
+
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, 'client/build')));
+
+// Catch-all handler for all routes (i.e., for client-side routing by React Router)
+app.get('/api/users', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+});
 
 module.exports = app;
--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const app = require('./app');
 const mongoose = require('mongoose');
 require('dotenv').config();
 
+
+
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
     app.listen(process.env.PORT, () => {
